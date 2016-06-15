7e5b555b96d6983f9be16a1c0b61c6aebd37922b
davidedelerma <davide.delerma@gmail.com>
2016-06-15 14:33:17 +0100

    added viewer

---
+++ viewer.rb
@@ -0,0 +1,22 @@
+class Viewer
+
+  def get_player_name(player_number)
+    puts "Player #{player_number}: Enter name"
+    return gets.chomp
+  end
+
+  def start(player_name)
+    puts
+    puts "#{player_name} roll dice!"
+    gets.chomp
+  end
+
+  def show_update(entry)
+    puts "#{entry.player.name} rolled #{entry.player.turn}"
+
+  end
+
+  def end(winner_name)
+    puts "Congratulations, #{winner_name} wins"
+  end
+end
